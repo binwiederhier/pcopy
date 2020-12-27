510213e59ef838b43cec3e4553ff1909306c6e5b
Philipp Heckel <pheckel@datto.com>
2020-12-27 15:29:41 -0500

    Add more limiter tests

--- limit_test.go
+++ limit_test.go
@@ -1,6 +1,9 @@
 package pcopy
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestLimiter_Add(t *testing.T) {
 	l := newLimiter(10)
@@ -24,5 +27,59 @@ func TestLimiter_AddSet(t *testing.T) {
 	l.Set(7)
 	if l.Value() != 7 {
 		t.Errorf("expected value to be %d, got %d", 7, l.Value())
+	}
+}
+
+func TestLimitWriter_WriteNoLimiter(t *testing.T) {
+	var buf bytes.Buffer
+	lw := newLimitWriter(&buf)
+	if _, err := lw.Write(make([]byte, 10)); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := lw.Write(make([]byte, 1)); err != nil {
+		t.Fatal(err)
+	}
+	if buf.Len() != 11 {
+		t.Errorf("expected buffer length to be %d, got %d", 11, buf.Len())
+	}
+}
+
+func TestLimitWriter_WriteOneLimiter(t *testing.T) {
+	var buf bytes.Buffer
+	l := newLimiter(10)
+	lw := newLimitWriter(&buf, l)
+	if _, err := lw.Write(make([]byte, 10)); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := lw.Write(make([]byte, 1)); err != limitReachedError {
+		t.Errorf("expected limitReachedError, got %#v", err)
+	}
+	if buf.Len() != 10 {
+		t.Errorf("expected buffer length to be %d, got %d", 10, buf.Len())
+	}
+	if l.Value() != 10 {
+		t.Errorf("expected limiter value to be %d, got %d", 10, l.Value())
+	}
+}
+
+func TestLimitWriter_WriteTwoLimiters(t *testing.T) {
+	var buf bytes.Buffer
+	l1 := newLimiter(11)
+	l2 := newLimiter(9)
+	lw := newLimitWriter(&buf, l1, l2)
+	if _, err := lw.Write(make([]byte, 8)); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := lw.Write(make([]byte, 2)); err != limitReachedError {
+		t.Errorf("expected limitReachedError, got %#v", err)
+	}
+	if buf.Len() != 8 {
+		t.Errorf("expected buffer length to be %d, got %d", 8, buf.Len())
+	}
+	if l1.Value() != 8 {
+		t.Errorf("expected limiter 1 value to be %d, got %d", 8, l1.Value())
+	}
+	if l2.Value() != 8 {
+		t.Errorf("expected limiter 2 value to be %d, got %d", 8, l2.Value())
 	}
 }
