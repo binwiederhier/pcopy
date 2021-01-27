669216326fe0d3ed0ae0c5e0171f154ed6696837
Philipp Heckel <pheckel@datto.com>
2021-01-27 09:22:36 -0500

    This sorta works

--- server_router.go
+++ server_router.go
@@ -16,8 +16,8 @@ import (
 type ServerRouter struct {
 	servers  []*Server
 	handler  http.Handler
-	srvHTTP  *http.Server
-	srvHTTPS *http.Server
+	srvHTTP  []*http.Server
+	srvHTTPS []*http.Server
 	mu       sync.Mutex
 }
 
@@ -41,8 +41,8 @@ func Serve(configs ...*Config) error {
 // If more than one config is passed, the "ListenAddr" configuration setting must be identical for all of them.
 func NewServerRouter(configs ...*Config) (*ServerRouter, error) {
 	var err error
-	if err := checkConfigs(configs); err != nil {
-		return nil, err
+	if len(configs) == 0 {
+		return nil, errInvalidNumberOfConfigs
 	}
 	servers, err := createServers(configs)
 	if err != nil {
@@ -55,49 +55,53 @@ func NewServerRouter(configs ...*Config) (*ServerRouter, error) {
 	return &ServerRouter{servers: servers, handler: handler}, nil
 }
 
+/*
+config 1: :443/https :80/http
+config 2: :1234/https
+config 3: :2586/https
+*/
+
 // Start starts the HTTP(S) server
 func (r *ServerRouter) Start() error {
+	r.mu.Lock()
+
+	var err error
+	r.srvHTTP, err = r.createHTTPServers(r.servers)
+	if err != nil {
+		return err
+	}
+	httpsServers, err := r.createHTTPSServerSpecs(r.servers)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%#v\n", r.srvHTTP)
+	log.Printf("%#v\n", httpsServers)
 	r.printListenInfo()
+
 	errChan := make(chan error)
-	if r.servers[0].config.ListenHTTP != "" {
-		r.mu.Lock()
-		r.srvHTTP = &http.Server{Addr: r.servers[0].config.ListenHTTP, Handler: r.handler}
-		r.mu.Unlock()
-		go func() {
-			if err := r.srvHTTP.ListenAndServe(); err != nil {
+	for _, s := range r.srvHTTP {
+		go func(s *http.Server) {
+			if err := s.ListenAndServe(); err != nil {
 				errChan <- err
 			}
-		}()
+		}(s)
 	}
-	if r.servers[0].config.ListenHTTPS != "" {
-		r.mu.Lock()
-		r.srvHTTPS = &http.Server{Addr: r.servers[0].config.ListenHTTPS, Handler: r.handler}
-		r.mu.Unlock()
-		go func() {
-			tlsConfig := &tls.Config{Certificates: make([]tls.Certificate, len(r.servers))}
-			for i, s := range r.servers {
-				var err error
-				tlsConfig.Certificates[i], err = tls.LoadX509KeyPair(s.config.CertFile, s.config.KeyFile)
-				if err != nil {
-					errChan <- err
-					return
-				}
-			}
-			listener, err := tls.Listen("tcp", r.servers[0].config.ListenHTTPS, tlsConfig)
+	for _, s := range httpsServers {
+		go func(s *httpsServerSpec) {
+			listener, err := tls.Listen("tcp", s.server.Addr, s.tlsConfig)
 			if err != nil {
 				errChan <- err
 				return
 			}
-			if err := r.srvHTTPS.Serve(listener); err != nil {
+			if err := s.server.Serve(listener); err != nil {
 				errChan <- err
-				return
 			}
-		}()
+		}(s)
 	}
-	for _, server := range r.servers {
-		go server.serverManager()
-	}
-	err := <-errChan
+
+	r.mu.Unlock()
+	err = <-errChan
 	return err
 }
 
@@ -106,13 +110,17 @@ func (r *ServerRouter) Stop() error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	if r.srvHTTP != nil {
-		if err := r.srvHTTP.Close(); err != nil {
-			return err
+		for _, s := range r.srvHTTP {
+			if err := s.Close(); err != nil {
+				return err
+			}
 		}
 	}
 	if r.srvHTTPS != nil {
-		if err := r.srvHTTPS.Close(); err != nil {
-			return err
+		for _, s := range r.srvHTTPS {
+			if err := s.Close(); err != nil {
+				return err
+			}
 		}
 	}
 	r.srvHTTP = nil
@@ -129,6 +137,72 @@ func (r *ServerRouter) printListenInfo() {
 		listens = append(listens, fmt.Sprintf("%s/https", r.servers[0].config.ListenHTTPS))
 	}
 	log.Printf("Listening on %s (%d clipboard(s))\n", strings.Join(listens, " "), len(r.servers))
+}
+
+type httpsServerSpec struct {
+	server    *http.Server
+	tlsConfig *tls.Config
+}
+
+func (r *ServerRouter) createHTTPSServerSpecs(servers []*Server) ([]*httpsServerSpec, error) {
+	var ok bool
+	var spec *httpsServerSpec
+	listenSpecs := make(map[string]*httpsServerSpec, 0)
+	for _, s := range servers {
+		if s.config.ListenHTTPS == "" {
+			continue
+		}
+		if spec, ok = listenSpecs[s.config.ListenHTTPS]; !ok {
+			spec = &httpsServerSpec{
+				server:    &http.Server{Addr: s.config.ListenHTTPS, Handler: http.NewServeMux()},
+				tlsConfig: &tls.Config{Certificates: make([]tls.Certificate, 0)},
+			}
+			listenSpecs[s.config.ListenHTTPS] = spec
+		}
+		serverURL, err := url.ParseRequestURI(ExpandServerAddr(s.config.ServerAddr))
+		if err != nil {
+			return nil, err
+		}
+		handler := spec.server.Handler.(*http.ServeMux)
+		handler.HandleFunc(fmt.Sprintf("%s/", serverURL.Hostname()), s.handle)
+
+		cert, err := tls.LoadX509KeyPair(s.config.CertFile, s.config.KeyFile)
+		if err != nil {
+			return nil, err
+		}
+		spec.tlsConfig.Certificates = append(spec.tlsConfig.Certificates, cert)
+	}
+	specs := make([]*httpsServerSpec, 0)
+	for _, spec := range listenSpecs {
+		specs = append(specs, spec)
+	}
+	return specs, nil
+}
+
+func (r *ServerRouter) createHTTPServers(servers []*Server) ([]*http.Server, error) {
+	var ok bool
+	var httpServer *http.Server
+	listenHTTPServers := make(map[string]*http.Server, 0)
+	for _, s := range servers {
+		if s.config.ListenHTTP == "" {
+			continue
+		}
+		if httpServer, ok = listenHTTPServers[s.config.ListenHTTP]; !ok {
+			httpServer = &http.Server{Addr: s.config.ListenHTTP, Handler: http.NewServeMux()}
+			listenHTTPServers[s.config.ListenHTTP] = httpServer
+		}
+		serverURL, err := url.ParseRequestURI(ExpandServerAddr(s.config.ServerAddr))
+		if err != nil {
+			return nil, err
+		}
+		handler := httpServer.Handler.(*http.ServeMux)
+		handler.HandleFunc(fmt.Sprintf("%s/", serverURL.Hostname()), s.handle)
+	}
+	httpServers := make([]*http.Server, 0)
+	for _, s := range listenHTTPServers {
+		httpServers = append(httpServers, s)
+	}
+	return httpServers, nil
 }
 
 func createHandler(servers []*Server) (*http.ServeMux, error) {
@@ -153,21 +227,6 @@ func createServers(configs []*Config) ([]*Server, error) {
 		}
 	}
 	return servers, nil
-}
-
-func checkConfigs(configs []*Config) error {
-	if len(configs) == 0 {
-		return errInvalidNumberOfConfigs
-	}
-	for _, config := range configs {
-		if config.ListenHTTP != configs[0].ListenHTTP {
-			return errors.New("config setting 'ListenHTTP' must be identical in all config files")
-		}
-		if config.ListenHTTPS != configs[0].ListenHTTPS {
-			return errors.New("config setting 'ListenHTTP' must be identical in all config files")
-		}
-	}
-	return nil
 }
 
 var errInvalidNumberOfConfigs = errors.New("invalid number of configs, need at least one")
