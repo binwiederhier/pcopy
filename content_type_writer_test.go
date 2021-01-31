69c7a5fd599babc934fe9a9178dd475765a29bb9
Philipp Heckel <pheckel@datto.com>
2021-01-30 19:22:41 -0500

    Add more tests for content type writer

--- content_type_writer_test.go
+++ content_type_writer_test.go
@@ -39,11 +39,37 @@ func TestSniffWriter_WriteHTMLSplitIntoTwoWrites(t *testing.T) {
 	assertStrEquals(t, "text/plain; charset=utf-8", rr.Header().Get("Content-Type"))
 }
 
-func TestSniffWriter_WriteBinary(t *testing.T) {
+func TestSniffWriter_WriteUnknownMimeType(t *testing.T) {
 	rr := httptest.NewRecorder()
 	sw := newContentTypeWriter(rr, "", false)
 	randomBytes := make([]byte, 199)
 	rand.Read(randomBytes)
 	sw.Write(randomBytes)
 	assertStrEquals(t, "application/octet-stream", rr.Header().Get("Content-Type"))
+}
+
+func TestSniffWriter_DownloadWithFilename(t *testing.T) {
+	rr := httptest.NewRecorder()
+	sw := newContentTypeWriter(rr, "some file", true)
+	sw.Write([]byte("this is a text file"))
+	assertStrEquals(t, "text/plain; charset=utf-8", rr.Header().Get("Content-Type"))
+	assertStrEquals(t, `attachment; filename="some file.txt"`, rr.Header().Get("Content-Disposition"))
+}
+
+func TestSniffWriter_DownloadWithSpecialCharFilename(t *testing.T) {
+	rr := httptest.NewRecorder()
+	sw := newContentTypeWriter(rr, "öäü().pdf", true)
+	sw.Write([]byte{0x25, 0x50, 0x44, 0x46, 0x2d, 0x11, 0x22, 0x33})
+	assertStrEquals(t, "application/pdf", rr.Header().Get("Content-Type"))
+	assertStrEquals(t, `attachment; filename*=utf-8''%C3%B6%C3%A4%C3%BC%28%29.pdf`, rr.Header().Get("Content-Disposition"))
+}
+
+func TestSniffWriter_DownloadWithUnknownMimeType(t *testing.T) {
+	rr := httptest.NewRecorder()
+	sw := newContentTypeWriter(rr, "abcdef", true)
+	randomBytes := make([]byte, 199)
+	rand.Read(randomBytes)
+	sw.Write(randomBytes)
+	assertStrEquals(t, "application/octet-stream", rr.Header().Get("Content-Type"))
+	assertStrEquals(t, `attachment; filename=abcdef.bin`, rr.Header().Get("Content-Disposition"))
 }
